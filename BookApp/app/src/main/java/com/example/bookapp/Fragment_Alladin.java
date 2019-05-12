91b64d9af4b5c7c1f9cd48a4a4f1530e37453d79
dkrlto0819 <enqn_k@naver.com>
2019-05-13 00:53:17 +0900

    make 3Fragment

---
+++ BookApp/app/src/main/java/com/example/bookapp/Fragment_Alladin.java
@@ -0,0 +1,6 @@
+package com.example.bookapp;
+
+import android.support.v4.app.Fragment;
+
+public class Fragment_Alladin extends Fragment {
+}
---
+++ BookApp/app/src/main/java/com/example/bookapp/Fragment_Yes24.java
@@ -0,0 +1,6 @@
+package com.example.bookapp;
+
+import android.support.v4.app.Fragment;
+
+public class Fragment_Yes24 extends Fragment {
+}
