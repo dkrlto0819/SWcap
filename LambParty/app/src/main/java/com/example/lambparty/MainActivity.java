364a189e68b653572a47e33c4995ee8e9275a679
dkrlto0819 <enqn_k@naver.com>
2019-05-19 14:24:01 +0900

    loading 화면 연결

--- LambParty/app/src/main/java/com/example/lambparty/MainActivity.java
+++ LambParty/app/src/main/java/com/example/lambparty/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lambparty;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -21,6 +22,9 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent =new Intent(this, LoadingActivity.class);
+        startActivity(intent);
+        
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
