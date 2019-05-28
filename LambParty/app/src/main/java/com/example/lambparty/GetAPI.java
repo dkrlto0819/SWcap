933a5338953a05f43fb120497c231ec5dde6621a
dkrlto0819 <enqn_k@naver.com>
2019-05-28 14:56:04 +0900

    주석 없앰

--- LambParty/app/src/main/java/com/example/lambparty/GetAPI.java
+++ LambParty/app/src/main/java/com/example/lambparty/GetAPI.java
@@ -14,56 +14,8 @@ import java.net.URLEncoder;
 
 public class GetAPI{
 
-
     public void getAPI(){
-
         new BackgroundTask().execute();
-
-//        String clientId = BuildConfig.LAMB_API_KEY;//애플리케이션 클라이언트 아이디값";
-//        String clientSecret = BuildConfig.LAMB_API_CLIENT;//애플리케이션 클라이언트 시크릿값";
-//
-//        try {
-//
-//            String text = URLEncoder.encode("토비의 스프링", "UTF-8");
-//
-//            String apiURL = "https://openapi.naver.com/v1/search/book.json?query="+ text; // json 결과
-//            URL url = new URL(apiURL);
-//
-//            Log.d(this.getClass().getName(), "URL : " + apiURL);
-//
-//            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-//
-//            con.setRequestMethod("GET");
-//
-//////
-////            con.setRequestMethod(con.setRequestMethod("GET");
-//            con.setRequestProperty("X-Naver-Client-Id", clientId);
-//            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
-//
-//            int responseCode = con.getResponseCode();
-//////            BufferedReader br;
-////            Log.d(this.getClass().getName(), "responseCode : " + responseCode);
-//
-////            if(responseCode==200) { // 정상 호출
-////
-////                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-////            } else {  // 에러 발생
-////                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-////            }
-////
-////            String inputLine;
-////
-////            StringBuffer response = new StringBuffer();
-////            while ((inputLine = br.readLine()) != null) {
-////                response.append(inputLine);
-////            }
-////            br.close();
-////            System.out.println("API Result : " + response.toString());
-////            Log.d(this.getClass().getName(), "API Result : " + response.toString());
-//        } catch (Exception e) {
-//            Log.d(this.getClass().getName(), "Fail");
-//            System.out.println(e);
-//        }
     }
 
     class BackgroundTask extends AsyncTask <Void, Void, Void>{
