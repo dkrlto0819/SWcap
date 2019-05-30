e9904d46e753a2599e902f17b1e8b6473ae6956d
dkrlto0819 <enqn_k@naver.com>
2019-05-31 02:45:19 +0900

    parse img src

--- LambParty/app/src/main/java/com/example/lambparty/Fragment_Home.java
+++ LambParty/app/src/main/java/com/example/lambparty/Fragment_Home.java
@@ -1,15 +1,44 @@
 package com.example.lambparty;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.annotation.Documented;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class Fragment_Home extends Fragment {
+
+    View rootView;
+    Context context;
+
+    ImageView homeImageView;
+    TextView bookTitle;
+    TextView bookAuthor;
+    TextView bookContent;
+
+    Bitmap bitmap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +49,51 @@ public class Fragment_Home extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        context=getActivity();
+
+        new webCrawling().execute();
+        return rootView;
+    }
+
+    class webCrawling extends AsyncTask<Void, Void, Void>{
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Document document = Jsoup.connect("https://book.naver.com/bestsell/bestseller_list.nhn").get();
+                Element element = document.select("div.thumb_type a img").first();
+                String img = element.attr("src");
+
+                URL imgUrl = new URL(img);
+
+                HttpURLConnection conn = (HttpURLConnection) imgUrl.openConnection();
+                conn.setDoInput(true);
+                conn.connect();
+
+                InputStream is = conn.getInputStream();
+                bitmap= BitmapFactory.decodeStream(is);
+
+                homeImageView = (ImageView)getActivity().findViewById(R.id.home_bookImage);
+                homeImageView.setImageBitmap(bitmap);
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Thread mThread = new Thread(){
+                @Override
+                public void run(){
+
+                }
+            }
+
+            return null;
+        }
     }
 }
