9d400e8e3f4fd41922c1f38285433b2f94ce45ef
dkrlto0819 <enqn_k@naver.com>
2019-05-31 17:13:31 +0900

    이미지 크롤링 성공

--- LambParty/app/src/main/java/com/example/lambparty/Fragment_Home.java
+++ LambParty/app/src/main/java/com/example/lambparty/Fragment_Home.java
@@ -3,10 +3,8 @@ package com.example.lambparty;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.renderscript.ScriptGroup;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,13 +16,10 @@ import android.widget.TextView;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.annotation.Documented;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 
@@ -54,19 +49,20 @@ public class Fragment_Home extends Fragment {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_home, container, false);
         context=getActivity();
-        homeImageView = (ImageView)getActivity().findViewById(R.id.home_bookImage);
+//        homeImageView = (ImageView)getActivity().findViewById(R.id.home_bookImage);
         new webCrawling().execute();
         return rootView;
     }
 
-    class webCrawling extends AsyncTask<Void, Void, Void>{
+    class webCrawling extends AsyncTask<Void, Void, Bitmap>{
+
         @Override
         protected void onPreExecute(){
             super.onPreExecute();
         }
 
         @Override
-        protected Void doInBackground(Void... voids) {
+        protected Bitmap doInBackground(Void... voids) {
             try {
                 Document document = Jsoup.connect("https://book.naver.com/bestsell/bestseller_list.nhn").get();
                 Element element = document.select("div.thumb_type a img").first();
@@ -80,14 +76,19 @@ public class Fragment_Home extends Fragment {
 
                 InputStream is = connection.getInputStream();
                 bitmap = BitmapFactory.decodeStream(is);
-                Log.d(this.getClass().getName(), "bitmap : " + bitmap);
-                //homeImageView.setImageBitmap(bitmap);
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            return null;
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bookImage){
+            Log.d(this.getClass().getName(), "bookImage : " + bookImage);
+            homeImageView = (ImageView)getActivity().findViewById(R.id.home_bookImage);
+            homeImageView.setImageBitmap(bookImage);
         }
     }
 }
