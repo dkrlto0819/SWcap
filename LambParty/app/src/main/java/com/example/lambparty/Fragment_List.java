c68f56259babb5e6213c54f0716d19ed85747ade
dkrlto0819 <enqn_k@naver.com>
2019-06-01 17:11:52 +0900

    json 데이터로부터 리스트 화면 구성

--- LambParty/app/src/main/java/com/example/lambparty/Fragment_List.java
+++ LambParty/app/src/main/java/com/example/lambparty/Fragment_List.java
@@ -2,16 +2,30 @@ package com.example.lambparty;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.simple.parser.ParseException;
+import org.w3c.dom.Text;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.ExecutionException;
 
 @SuppressLint("ValidFragment")
@@ -22,6 +36,21 @@ public class Fragment_List extends Fragment {
 
     String searchText;
     String jsonData;
+
+    TextView title;
+    TextView author;
+    TextView content;
+    ImageView bookImage;
+
+    Bitmap bitmap;
+    String imgUrl;
+
+    int titleID;
+    int authorID;
+    int contentID;
+    int imageID;
+
+    String resultData[][];
 
     @SuppressLint("ValidFragment")
     Fragment_List(String searchText){
@@ -50,16 +79,93 @@ public class Fragment_List extends Fragment {
         }
 
         Log.d(this.getClass().getName(), "This is json Data : " + jsonData);
+        resultData = new String[10][10];
         ProcessJson processJson = new ProcessJson();
-
         try {
-            processJson.jsonParse(jsonData);
+            resultData = processJson.jsonParse(jsonData);
         } catch (ParseException e) {
             e.printStackTrace();
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
+        for(int i=1;i<=3;i++){
+//            String titleString = "list_bookTitle" + i;
+//            String authorString = "list_bookAuthor" + i;
+//            String contentString = "list_bookContent" + i;
+//            String imageString = "list_bookImage" + i;
+
+            imgUrl = resultData[3][i-1];
+
+            Thread mThread = new Thread(){
+                @Override
+                public void run(){
+                    try{
+                        URL url = new URL(imgUrl);
+                        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                        connection.setDoInput(true);
+                        connection.connect();
+
+                        InputStream is = connection.getInputStream();
+                        bitmap = BitmapFactory.decodeStream(is);
+
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            };
+
+            mThread.start();
+            try{
+                mThread.join();
+                new moveToBannerWeb().execute(i);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         return rootView;
     }
+
+    class moveToBannerWeb extends AsyncTask<Integer, Void, Integer> {
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Integer doInBackground(Integer... index) {
+            return index[0];
+        }
+
+        @Override
+        protected void onPostExecute(Integer result){
+            String titleString = "list_bookTitle" + result;
+            String authorString = "list_bookAuthor" + result;
+            String contentString = "list_bookContent" + result;
+            String imageString = "list_bookImage" + result;
+
+            titleID = getResources().getIdentifier(titleString, "id", getActivity().getPackageName());
+            authorID = getResources().getIdentifier(authorString, "id", getActivity().getPackageName());
+            contentID = getResources().getIdentifier(contentString, "id", getActivity().getPackageName());
+            imageID = getResources().getIdentifier(imageString, "id", getActivity().getPackageName());
+
+            title = (TextView)getActivity().findViewById(titleID);
+            author = (TextView)getActivity().findViewById(authorID);
+            content = (TextView)getActivity().findViewById(contentID);
+            bookImage = (ImageView)getActivity().findViewById(imageID);
+
+            Log.d(this.getClass().getName(), "This is title : "+ title);
+
+            title.setText(resultData[0][result-1]);
+            author.setText(resultData[1][result-1]);
+            content.setText(resultData[2][result-1]);
+            bookImage.setImageBitmap(bitmap);
+        }
+    }
+
 }
--- LambParty/app/src/main/java/com/example/lambparty/ProcessJson.java
+++ LambParty/app/src/main/java/com/example/lambparty/ProcessJson.java
@@ -9,19 +9,27 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class ProcessJson {
-    public void jsonParse(String jsonStr) throws ParseException, JSONException {
+    public String[][] jsonParse(String jsonStr) throws ParseException, JSONException {
         JSONParser jsonParser = new JSONParser();
+        String[][] resultData = new String[4][3];
 
         Log.d(this.getClass().getName(), jsonStr);
         org.json.simple.JSONObject jsonObject = (org.json.simple.JSONObject) jsonParser.parse(jsonStr);
         org.json.simple.JSONArray jsonArray = (org.json.simple.JSONArray) jsonObject.get("items");
 
-        for(int i=0;i<jsonArray.size();i++){
+        for(int i=0;i<3;i++){
             org.json.simple.JSONObject tempObj = (org.json.simple.JSONObject) jsonArray.get(i);
             String title = tempObj.get("title").toString().replaceAll("<(/)?([a-zA-Z]*)(\\s[a-zA-Z]*=[^>]*)?(\\s)*(/)?>", "");
+            String description = tempObj.get("description").toString().replaceAll("<(/)?([a-zA-Z]*)(\\s[a-zA-Z]*=[^>]*)?(\\s)*(/)?>", "");
                 // <b> 제거 정규식
+            resultData[0][i] = title;
+            resultData[1][i] = tempObj.get("author").toString();
+            resultData[2][i] = description;
+            resultData[3][i] = tempObj.get("image").toString();
+
             Log.d(this.getClass().getName(), "title : " + title);
             Log.d(this.getClass().getName(), "price : "+ tempObj.get("price").toString());
         }
+        return resultData;
     }
 }
